834c3e3276b5b1107417b45a0554c72e2587cc21
423803 <423803@m.mie-u.ac.jp>
2025-09-08 14:03:54 +0900

    レシピ

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def send_api():
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": received_text}
             ], # type: ignore
-            model="google/gemma-3-27b-it:free", 
+            model="google/gemma-2-9b-it:free", 
         )
         
         # APIからのレスポンスを取得
